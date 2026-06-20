4c852e7a4cc8518291ae068b5d2760e39c5494bc
Gabriel Oazem <gabrieloazemvieira@gmail.com>
2026-06-19 23:29:09 -0300

    .

--- index.js
+++ index.js
@@ -5,20 +5,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Endpoint 1
 app.get('/', (req, res) => {
   res.json({ message: "Hello World" });
 });
 
-// Endpoint 2
 app.get('/endpoint2', (req, res) => {
-  res.json({ message: "Hello endpoint2" });
-});
-
-// Exemplo de POST (para enviar dados)
-app.post('/enviar-dados', (req, res) => {
-  const dados = req.body;
-  res.json({ status: "sucesso", recebido: dados });
+  res.json({ message: "Endpoint 2" });
 });
 
 const port = process.env.PORT || 3000;
