ebb310850dc6d746923622622eac993d135976f7
Gabriel Oazem <gabrieloazemvieira@gmail.com>
2026-06-19 23:51:20 -0300

    .

--- index.js
+++ index.js
@@ -5,29 +5,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Função auxiliar para padronizar as respostas
-const sendResponse = (res, data, error = null, status = 200) => {
-  res.status(status).json({
-    success: error === null,
-    data: data,
-    error: error
-  });
-};
-
 app.get('/', (req, res) => {
-  res.json({ message: "Hello World" });
+  res.json({ 
+    sucess: true,
+    data: "Hello World" 
+  });
 });
 
 app.get('/endpoint1', (req, res) => {
-  sendResponse(res, 'Endpoint 1');
+  res.json({ 
+    sucess: true,
+    data: "Endpoint 1" 
+  });
 });
 
 app.get('/endpoint2', (req, res) => {
-  sendResponse(res, 'Endpoint 2');
-});
-
-app.get('/erro', (req, res) => {
-  sendResponse(res, null, 'Algo deu errado!', 400);
+  res.json({ 
+    sucess: true,
+    data: "Endpoint 2" 
+  });
 });
 
 const port = process.env.PORT || 3000;
